cceb2bd4f7b818bf6fa7f740ed8d75e42eed1c1c
ElvinaGallyamova <123301643+ElvinaGallyamova@users.noreply.github.com>
2023-01-22 20:57:04 +0300

    Create lesson 1 example 1.py

---
+++ lesson 1 example 1.py
@@ -0,0 +1,14 @@
+# Поработайте с переменными, создайте несколько, выведите на экран.
+# Запросите у пользователя некоторые числа и
+# строки и сохраните в переменные, затем выведите на экран.
+
+user_name: str = "Elvina"
+user_age = 23
+print(f"Hi, {user_name}")
+print(f"How old are you? {user_age}")
+age_1 = int(input("Enter your age, "))
+age_2 = int(input("Enter yor age, "))
+name_1 = input("Enter yor name, ")
+name_2 = input("Enter yor name, ")
+print(f"Good Morning, {user_name}, {name_1}, {name_2}")
+print(f"Your age is, {user_age}, {age_1}, {age_2} ")
